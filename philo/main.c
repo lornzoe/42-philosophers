c094904d61d54a49ff71ac0d7c4c648a52479c49
Lorenzo Yanga <lorenzoyanga@gmail.com>
2025-12-29 18:22:05 +0800

    arbitrary rest solution

--- philo/main.c
+++ philo/main.c
@@ -6,16 +6,17 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 01:59:17 by lyanga            #+#    #+#             */
-/*   Updated: 2025/12/21 20:09:10 by lyanga           ###   ########.fr       */
+/*   Updated: 2025/12/29 18:21:47 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // all the libs needed for the required funcs
-#include <bits/pthreadtypes.h>
 #include <inttypes.h>
+#include <stdint.h>
 #include <stdio.h> // printf
 #include <stdlib.h> // malloc, free
 #include <string.h> // memset
+#include <sys/types.h>
 #include <unistd.h> // write, usleep
 #include <sys/time.h> // gettimeofday
 #include <pthread.h> // pthread funcs (3) + pthread_mutex funcs (4)
@@ -38,7 +39,7 @@ struct philosopher
 	
 	int state;
 	int time_in_state;
-	int deadline;
+	u_int64_t deadline;
 	int times_eaten;
 	
 	int time_to_die;
@@ -46,6 +47,9 @@ struct philosopher
 	int time_to_sleep;
 	pthread_mutex_t *fork_left;
 	pthread_mutex_t *fork_right;
+
+	pthread_mutex_t *death_check;
+	u_int64_t start;
 };
 
 struct sim_info
@@ -56,12 +60,15 @@ struct sim_info
 	int time_to_sleep;
 	int minimum_eats;
 	pthread_mutex_t *forks;
+	pthread_mutex_t death_check;
 };
-
-static int dt(struct philosopher *philo)
+static uint64_t get_time(uint64_t start)
 {
-	// get delta time with this
-	return 0;
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+	uint64_t time = (uint64_t)tv.tv_sec * 1000 + (tv.tv_usec / 1000);
+	// printf("[timestamp: %lu]\n", time);
+    return ((uint64_t)tv.tv_sec * 1000 + (tv.tv_usec / 1000) - start);
 }
 
 static int argc_fail(void)
@@ -69,10 +76,9 @@ static int argc_fail(void)
 	printf("usage: number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\n");
 	return EXIT_FAILURE;
 }
-static void init_a_philo(struct philosopher *philo, int time_to_die, struct sim_info *info, int i)
+static void init_a_philo(struct philosopher *philo, struct sim_info *info, int i)
 {
 	philo->state = THINK;
-	philo->deadline = time_to_die;
 	philo->time_in_state = 0;
 	philo->times_eaten = 0;
 
@@ -86,6 +92,7 @@ static void init_a_philo(struct philosopher *philo, int time_to_die, struct sim_
 		philo->fork_right = &(info->forks[0]);
 	else
 	 	philo->fork_right = &(info->forks[i + 1]);
+	philo->death_check = &(info->death_check);
 }
 
 static int init_setup(struct sim_info *info, struct philosopher **philosophers, int argc, char **argv)
@@ -107,38 +114,80 @@ static int init_setup(struct sim_info *info, struct philosopher **philosophers, 
 		return FUNC_FAIL;
 	i = 0;
 	while (i < info->num)
+	{
 		pthread_mutex_init(&(info->forks[i]), NULL);
+		i++;
+	}
+	pthread_mutex_init(&(info->death_check), NULL);
 	i = 0;
 	while (i < info->num)
-		init_a_philo(philosophers[i], info->time_to_die, info, i++);
+	{
+		init_a_philo(&((*philosophers)[i]), info, i);
+		i++;
+	}
 	
-	printf("[SIMULATION INFO]\n");
-	printf("Number of philosophers: %d\n", info->num);
-	printf("Time to die: %d seconds\n", info->time_to_die);
-	printf("Time to sleep: %d seconds\n", info->time_to_sleep);
-	printf("Time to eat: %d seconds\n", info->time_to_eat);
-	printf("-----------------------\n");
+	// printf("[SIMULATION INFO]\n");
+	// printf("Number of philosophers: %d\n", info->num);
+	// printf("Time to die: %d ms\n", info->time_to_die);
+	// printf("Time to sleep: %d ms\n", info->time_to_sleep);
+	// printf("Time to eat: %d ms\n", info->time_to_eat);
+	// printf("-----------------------\n");
 	return FUNC_SUCCESS;
 }
 
 void *philosophise(void *args)
 {
-	struct philosopher philo;
-	philo = *((struct philosopher *)args);
+	struct philosopher *philo;
+	philo = (struct philosopher *)args;
+	philo->state = THINK;
+	usleep(philo->id % 2 * 50);
 	while (1)
 	{
-		philo.state = THINK;
-		pthread_mutex_lock(philo.fork_left);
-		pthread_mutex_lock(philo.fork_right);
-		
-		philo.state = EAT;
-		usleep(philo.time_to_eat * 1000000);
-		pthread_mutex_unlock(philo.fork_left);
-		pthread_mutex_unlock(philo.fork_right);
-		// extend deadline here
-		philo.state = SLEEP;
-		usleep(philo.time_to_sleep * 1000000);
+		if (philo->id % 2)
+		{
+			pthread_mutex_lock(philo->fork_left);
+			printf("%lu %d has taken a fork\n", get_time(philo->start), philo->id);
+			pthread_mutex_lock(philo->fork_right);
+		}
+		else
+		{
+			pthread_mutex_lock(philo->fork_right);
+			printf("%lu %d has taken a fork\n", get_time(philo->start), philo->id);
+			pthread_mutex_lock(philo->fork_left);
+		}
+		// i eat.
+		printf("%lu %d is eating\n", get_time(philo->start), philo->id);
+		philo->state = EAT;
+		// extend deadline
+		if (philo->deadline < get_time(philo->start))
+		{
+			philo->state = DEAD;
+			break ;
+		}
+		else
+			philo->deadline = get_time(philo->start) + philo->time_to_die;
+		usleep(philo->time_to_eat * 1000);
+		philo->times_eaten += 1;
+		pthread_mutex_unlock(philo->fork_left);
+		pthread_mutex_unlock(philo->fork_right);
 
+		// i sleep.
+		printf("%lu %d is sleeping\n", get_time(philo->start), philo->id);
+		philo->state = SLEEP;
+		usleep(philo->time_to_sleep * 1000);
+
+		// i check for death.
+		pthread_mutex_lock(philo->death_check);
+		pthread_mutex_unlock(philo->death_check);
+	
+		// i think.
+		printf("%lu %d is thinking\n", get_time(philo->start), philo->id);
+		philo->state = THINK;
+		// if sleep is shorter than eat time, then force an idle to ensure we do not get another collision
+		if (philo->time_to_sleep < philo->time_to_eat)
+			usleep((philo->time_to_eat - philo->time_to_sleep) * 1000);
+		// mandatory sleep time
+		usleep(1000);
 	}
 	return NULL;
 }
@@ -146,20 +195,67 @@ void *philosophise(void *args)
 int main(int argc, char **argv) 
 {
 	struct sim_info info;
-	struct philosopher *philosophers;	
-	printf("argc: %d\n", argc);
+	struct philosopher *philosophers;
+	int death;
+	uint64_t start;
+
+
+	death = 0;
+	// printf("argc: %d\n", argc);
 	if (argc < 5 || argc > 6)
 		return argc_fail();
 	if (!init_setup(&info, &philosophers, argc, argv))
+	{
 		return (EXIT_FAILURE);
-	// ready set go!
+	}
+	start = get_time(0);
 	int i = 0;
 	while (i < info.num)
-		philosophers[i].id = pthread_create(&(philosophers[i].thread), NULL, philosophise, &(philosophers[i]));
+	{
+		philosophers[i].id = i + 1;
+		philosophers[i].start = start;
+		philosophers[i].deadline = info.time_to_die;
+		pthread_create(&(philosophers[i].thread), NULL, philosophise, &(philosophers[i]));
+		pthread_detach(philosophers[i].thread);
+		i++;
+	}
 	// logic start
-
+	while (1)
+	{
+		i = 0;
+		while (i < info.num)
+		{
+			if (philosophers[i].state == DEAD || philosophers[i].deadline < get_time(philosophers[i].start))
+			{
+				printf("%lu %d died\n", get_time(start), philosophers[i].id);
+				death = 1;
+				break;
+			}
+			i++;
+		}
+		if (death)
+			break;
+		if (info.minimum_eats > -1)
+		{
+			i = 0;
+			while (i < info.num)
+			{
+				if (philosophers[i].times_eaten < info.minimum_eats)
+					break ;
+				if (i == info.num - 1)
+					death = 1;
+				i++;
+			}
+			if (death)
+				break;
+		}
+	}
 	// logic end
 	// cleanup
+	if (death)
+		pthread_mutex_lock(&(info.death_check));
 	free(philosophers);
+	pthread_mutex_destroy(&(info.death_check));
+	// the freeing process needs to be more elaborate here
 	return (EXIT_SUCCESS);
 }

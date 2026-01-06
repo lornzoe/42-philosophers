ce12084d534dbd8f6cf8ca1c972c06fa56db39c1
Lorenzo <lorenzoyanga@gmail.com>
2026-01-06 18:14:06 +0800

    Move sleep_min code to main

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 01:59:17 by lyanga            #+#    #+#             */
-/*   Updated: 2025/12/29 18:22:54 by lyanga           ###   ########.fr       */
+/*   Updated: 2026/01/06 17:02:48 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -135,6 +135,20 @@ static int init_setup(struct sim_info *info, struct philosopher **philosophers, 
 	return FUNC_SUCCESS;
 }
 
+// bufferUS is a constant buffer time in microseconds that is meant to give the program leeway time to act
+#define BUFFERUS 100
+
+void sleep_for(uint64_t sleepeatdiff, uint64_t timeleft, uint64_t percentage, uint64_t ttd)
+{
+    // if time to sleep is shorter than time to eat, minimum time to usleep should be the diff + BUFFERUS
+    if (sleepeatdiff > 0)
+        usleep(sleepeatdiff * 1000);
+    if ((timeleft / 100 * percentage < ttd))
+        usleep(ttd * 1000 - BUFFERUS);
+    else
+        usleep(timeleft * 10 * percentage - BUFFERUS);
+}
+
 void *philosophise(void *args)
 {
 	struct philosopher *philo;
@@ -183,11 +197,7 @@ void *philosophise(void *args)
 		// i think.
 		printf("%lu %d is thinking\n", get_time(philo->start), philo->id);
 		philo->state = THINK;
-		// if sleep is shorter than eat time, then force an idle to ensure we do not get another collision
-		if (philo->time_to_sleep < philo->time_to_eat)
-			usleep((philo->time_to_eat - philo->time_to_sleep) * 1000);
-		// mandatory sleep time
-		usleep(100);
+		sleep_for(philo->time_to_eat - philo->time_to_sleep, get_time(philo->start) - philo->deadline, 10, philo->time_to_die);
 	}
 	return NULL;
 }
